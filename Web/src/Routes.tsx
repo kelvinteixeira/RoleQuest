9198214949d5c9ba8430c4297fdd31aa2dd3e2dc
kesantos <kesantos@paschoalotto.com.br>
2023-08-08 12:23:26 -0300

    feat: Create: singup component

--- Web/src/Routes.tsx
+++ Web/src/Routes.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login } from "./screens/Login/Login";
+import { SingIn } from "./screens/SingIn/SingIn";
 import { Almanac } from "./components/Almanac/Almanac";
 import { PlayerFiles } from "./components/PlayerFiles/PlayerFiles";
 import { Campaing } from "./components/Campaing/Campaing";
 import { Header } from "./components/Header/Header";
 import { useAppSelector } from "./redux/hooks/hooks";
+import { SingUp } from "./screens/SingUp/SingUp";
 
 export const AppRoutes = () => {
   const user = useAppSelector((state) => state.authReducer.user);
@@ -13,7 +14,8 @@ export const AppRoutes = () => {
     <BrowserRouter>
       {user ? <Header />: null}
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<SingIn />} />
+        <Route path="/singup" element={<SingUp />} />
         <Route path="/campaing" element={<Campaing />} />
         <Route path="/almanac" element={<Almanac />} />
         <Route path="/playerFiles" element={<PlayerFiles />} />
